ab92483c264af4c4aee0e847b3f4c84ba1df9583
Conner Hogan <ConnerRHogan@gmail.com>
2022-02-20 02:46:45 -0700

    routes and controllers set up

--- back/server.js
+++ back/server.js
@@ -1,3 +1,27 @@
-// 9 mins in
+// 28 mins
+// main server
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5001
+const {errorHandler} = require('./middleWare/errorMiddleWare')
 
-console.log("hello");
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+// home route.
+app.get('/', (req, res)=>{
+    res.status(200).send(`home`)
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`);
+})
